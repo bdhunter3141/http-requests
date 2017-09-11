5bc844215a551fe78ab89f36cfdc7a1f7d54252b
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-11 16:07:35 +0000

    Fully modular with new file calling it

---
+++ http-functions.js
@@ -0,0 +1,14 @@
+var https = require("https");
+
+module.exports = function getHTML (options, callback) {
+  var result = "";
+  https.get(options, function(response) {
+    response.setEncoding("utf8");
+    response.on("data", function(data) {
+      result += data;
+    });
+    response.on("end", function() {
+      callback(result);
+    });
+  });
+};
