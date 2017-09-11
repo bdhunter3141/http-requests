39b984fdf4b7cb18b12b2453cead2f2dd64b6f65
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-11 15:32:11 +0000

    Created a modular version of the function

---
+++ modular-get-and-print-html.js
@@ -0,0 +1,21 @@
+function getAndPrintHTML (options) {
+  var https = require("https");
+  var result = "";
+  https.get(requestOptions, function(response) {
+    response.setEncoding("utf8");
+    response.on("data", function(data) {
+      result += data;
+      // console.log(data);
+    });
+    response.on("end", function() {
+      console.log(result);
+    });
+  });
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions);
